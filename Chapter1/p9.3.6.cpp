d8545904a9872830d1eb0d6ce0321ff6bead97d9
io888 <ioWork@foxmail.com>
2022-07-05 22:10:59 +0800

    p9.3.6 solved

--- Chapter1/p9.3.6.cpp
+++ Chapter1/p9.3.6.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <vector>
 #include <list>
 #include <forward_list>
 
 void func1();
+void func2();
+void func3();
 
 int main()
 {
-	func1();
+	//func1();
+	//func2();
+	func3();
+
+	return 0;
 }
 
 void func1()
@@ -17,14 +24,53 @@ void func1()
 	auto iter = l.begin();
 
 	while (iter != l.end()) {
-		if (*iter % 2)
+		if (*iter % 2) {
 			l.insert(iter, *iter);
+			++iter;
+		}
 		else
-			l.erase(iter);
-		++iter;
+			iter = l.erase(iter);
 	}
 
 	for (auto i : l)
+		std::cout << i << " ";
+	std::cout << std::endl;
+}
+
+void func2()
+{
+	std::vector<int> v;
+	for (int i = 0; i!=10; ++i)
+		v.emplace_back(i);
+
+	auto begin = v.begin(), end = v.end();
+	while (begin != v.end()) {
+		++begin;
+		v.insert(begin, 42);
+		++begin;
+	}
+
+	for (auto i : v)
+		std::cout << i << " ";
+	std::cout << std::endl;
+}
+
+void func3()
+{
+	std::vector<int> vi;
+	for (int i = 0; i!=10; ++i)
+		vi.emplace_back(i);
+
+	auto iter = vi.begin();
+	while (iter != vi.end()) {
+		if (*iter % 2) {
+			iter = vi.insert(iter, *iter);
+			++iter;
+		}
+		++iter;
+	}
+	
+	for (auto i : vi)
 		std::cout << i << " ";
 	std::cout << std::endl;
 }
