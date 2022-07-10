b0eaa66269dd6aeb56541993c86d3630a594b5f8
io888 <ioWork@foxmail.com>
2022-07-10 15:36:41 +0800

    p9.5.5 solved

--- Chapter9/p9.5.5.cpp
+++ Chapter9/p9.5.5.cpp
@@ -6,10 +6,10 @@
 class date {
 private:
 	std::map<std::string, int> monthMap = {
-		{"Jan", 0}, {"Janurary", 0}, {"Feb", 0}, {"February", 0}, {"Mar", 0}, {"March", 0}, 
-		{"Apr", 0}, {"April", 0}, {"May", 0}, {"May", 0}, {"Jun", 0}, {"June", 0}, 
-		{"Jul", 0}, {"July", 0}, {"Aug", 0}, {"August", 0}, {"Sep", 0}, {"September", 0}, 
-		{"Oct", 0}, {"October", 0}, {"Nov", 0}, {"November", 0}, {"Dec", 0}, {"December", 0}, 
+		{"Jan", 1}, {"Janurary", 1}, {"Feb", 2}, {"February", 2}, {"Mar", 3}, {"March", 3}, 
+		{"Apr", 4}, {"April", 4}, {"May", 5}, {"May", 5}, {"Jun", 6}, {"June", 6}, 
+		{"Jul", 7}, {"July", 7}, {"Aug", 8}, {"August", 8}, {"Sep", 9}, {"September", 9}, 
+		{"Oct", 10}, {"October", 10}, {"Nov", 11}, {"November", 11}, {"Dec", 12}, {"December", 12}, 
 	};
 public:
 	unsigned year = 1;
@@ -18,27 +18,26 @@ public:
 	date() { }
 	date(std::string d) {
 		std::string letters;
-		std::string numbers = "012345679";
+		std::string numbers = "0123456789";
 		for (int i = 0; i<26; ++i) {
 			letters += ('a' + i);
 			letters += ('A' + i);
 		}
 		std::string::size_type pos = 0;
 		if (d.find_first_of(letters) == 0) {
+			//the last position of MONTH
 			pos = d.find_last_of(letters);
-			
-			for (int i = 2; i<=24; ++i) {
-					
-			}
+			month = monthMap[d.substr(0, pos+1)];
+			day = std::stoi(d.substr(pos+2, d.find_first_not_of(numbers, pos+2)+1));
 		} else {
-			pos = d.find_first_not_of(numbers) - 1;
-			day = std::stoi(d.substr(pos+2, d.find_first_not_of(numbers, pos+2)));
+			pos = d.find_first_not_of(numbers);
+			day = std::stoi(d.substr(0, pos));
+			month = std::stoi(d.substr(pos+1, d.find_first_not_of(numbers, pos+1)-pos-1));
 		}
-		month = std::stoi(d.substr(0, pos));
-		year = std::stoi(d.substr(d.find_last_not_of(numbers)+1, d.size()-1));
+		year = std::stoi(d.substr(d.find_last_not_of(numbers)+1, 4));
 	}
 	std::ostream& print() {
-		std::cout << this->year << " " << this->month << " " << this->day;
+		std::cout << this->year << "Äê " << this->month << "ÔÂ " << this->day << "ÈÕ\n";
 		return std::cout;
 	}
 };
@@ -57,6 +56,10 @@ int main()
 
 	date day1("Jan 10 1988");
 	day1.print() << std::endl;
+	date day2("12/10/2022");
+	day2.print() << std::endl;
+	date day3("December 14,1999");
+	day3.print() << std::endl;
 
 	return 0;
 }
